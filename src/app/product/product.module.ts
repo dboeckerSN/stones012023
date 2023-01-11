2128d24ca6940500404095c65746a2099f6cd5f3
Dominik Böcker <dominik.boecker@sn-invent.de>
2023-01-11 15:58:53 +0100

    aufg 5

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductDisplayComponent } from './product-display/product-display.component';
 import { UtilsModule } from '../utils/utils.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [ProductDisplayComponent],
-  imports: [CommonModule, ProductRoutingModule, UtilsModule],
+  imports: [CommonModule, ProductRoutingModule, UtilsModule, MatButtonModule],
   exports: [ProductDisplayComponent],
 })
 export class ProductModule {}
